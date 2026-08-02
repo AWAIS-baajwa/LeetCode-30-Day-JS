371c24faa5a321db3a7a240f7f303518d30557f0
AWAIS-Baajwa <usmanintiezar@gmail.com>
2026-08-03 00:45:37 +0500

    Function Transformation Memoize

---
+++ Memoize.js
@@ -0,0 +1,19 @@
+// Runtime
+// 214  ms
+// Beats  99.57%
+
+var memoize = function (fn) {
+  const cache = new Map();
+
+  return function (...args) {
+    const key = JSON.stringify(args);
+
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
+
+    const result = fn(...args);
+    cache.set(key, result);
+    return result;
+  };
+};
